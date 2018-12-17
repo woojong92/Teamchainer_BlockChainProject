77dcfa4b97e48afd93f7251849a03ab7208e58ce
Woojong Kim <woojong92@gmail.com>
2018-12-17 15:47:26 +0900

    수정

--- Contract/AuctionFactory.sol
+++ Contract/AuctionFactory.sol
@@ -61,15 +61,16 @@ contract EstateAuction is Ownable {
         uint currentPrice;  //현재 낙찰가   
         uint startTime; //옥션 시작시간
         uint endTime;   //옥션 마감시간
-        bool finishAuction; //옥션 마감 확인
-        bool complete;  //옥션 거래 완료
     }
+
+    bool finishAuction = false; //옥션 마감 확인
+    bool complete = false;  //옥션 거래 완료
 
     mapping(address => uint) participationPrice;   //옥션참가자의 참가 토큰개수
     mapping(uint => address) auctioneer;    //낙찰 순서대로 맵핑
     uint auctioneerCount;
 
-    //Auction 구조체를 담는 배열
+    //Auction 구조체를 담는 변수
     Auction auction;
     
     
@@ -89,15 +90,6 @@ contract EstateAuction is Ownable {
 
     }
 
-    /*
-    function getSummary() public view returns (uint, uint, uint) {
-        return (
-            auction.estateId,
-            auction.currentPrice,
-            auction.closingDate
-        );
-    }*/
-
     //Auction 생성
     function createAuction(string memory _description, uint _estateId, uint _firstPrice, uint _endTime) public {
         auction = Auction({
@@ -106,28 +98,12 @@ contract EstateAuction is Ownable {
             firstPrice : _firstPrice,
             currentPrice : 0,
             startTime : now,
-            endTime : _endTime,
-            finishAuction : false,
-            complete : false
+            endTime : _endTime
         });
-
-        //auctions.push(newAuction);
     }
 
-    //참여가능한 옥션인가?
-    modifier inParticipationTime() {
-        require(
-            canParticipate(),
-            "This Auction is finished."
-        );
-        _;
-    }
 
-    //참여가능한 옥션인지 확인하는 함수
-    function canParticipate() public view returns (bool) {
-        Auction memory _auction = auction;
-        return now >= _auction.startTime && now <= _auction.endTime;
-    }
+
 
     //Auction 참가하기
     function joinAuction()  public payable inParticipationTime() returns(bool)  {
@@ -136,7 +112,7 @@ contract EstateAuction is Ownable {
             participationPrice[msg.sender] = msg.value;
             auctioneer[auctioneerCount] = msg.sender;
             auctioneerCount.add(1);
-            _auction.currentPrice = msg.sender;
+            _auction.currentPrice = msg.value;
             return true;
         } else {
             return false;
@@ -155,28 +131,53 @@ contract EstateAuction is Ownable {
         }
     }*/
 
+    //경매가 종료되고 입찰가 상위 3명 이외의 사람의 토큰 돌려주기
+    function returnToken() public {
+        //경매가 종료 되었는지 확인
+        require(finishAuction, "Auction is not finished.");
+        //상위 3명을 제외한 사람들의 토큰 순차적으로 돌려주기
+        for (uint i = 0 ; i < auctioneerCount; i++ ){
+            transfer(to, value);// ??? HOW???
+        }
+    }
+
+
     // multi-sig ?
     function consensus() public {
-
+    
     }
 
     //Auction 참가 종료시키기
     function closingAution() public returns(bool) {
-        Auction storage _auction = auction;
-        if( !canParticipate() ) {
-            _auction.finishAuction = true;
-            return true; //Auction 참가 기간 종료 
-        } else {
-            return false; //Auction 참가 기간 미종료
-        }    
+        finishAuction = true;
     }
 
     //거래 완료
     function completeAuction() public onlyOwner returns(bool) {
-        Auction storage _auction = auction;
-        _auction.complete = true;
-        return true;
+        return complete = true;        
     }
+
+    //참여가능한 옥션인가?
+    modifier inParticipationTime() {
+        require(
+            canParticipate(),
+            "This Auction is finished."
+        );
+        _;
+    }
+
+    //참여가능한 옥션인지 확인하는 함수
+    function canParticipate() public view returns (bool) {
+        Auction memory _auction = auction;
+        if( now >= _auction.startTime && now <= _auction.endTime ) {
+            return true;
+        } else {
+            //finishAuction = true;
+            return false;
+        }
+    }
+
+
 
 }
 
